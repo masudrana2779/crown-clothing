5b5f37e68affbfc593f7cc64499bf4ee3f108dbd
Masud Rana <masudrana2779@gmail.com>
2021-01-15 12:11:28 +0600

    oh I'm one of the most criminals

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
 
-const MenuItem = ({title}) => (    
-    <div className="menu-item">
+import './menu-item.style.scss';
+
+const MenuItem = ({item}) => (    
+    <div className={item.size ? `${item.size} menu-item` : 'menu-item' } key={item.id}>
+    <div className='bgImg' style={{backgroundImage:`url(${item.imageUrl})`}} />
     <div className="content">
-      <h1 className="title">{title}</h1>
+      <h1 className="title">{item.title}</h1>
       <span className="subtitle">SHOP NOW</span>
     </div>
   </div>
