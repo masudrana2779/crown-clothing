8aefd4e9bdd22128ac6685498f84886cf9d9f986
Masud Rana <masudrana2779@gmail.com>
2021-02-24 18:47:33 +0600

    our project added to a shop page

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import "./App.css";
 import HomePage from "./pages/homePage/HomePage.Component";
 import {Switch, Route} from 'react-router-dom';
 import ShopPagesComponent from "./pages/shopPage/shopPages.component";
+import Header from "./components/header/header";
 
 function App() {
     return (
         <div>
+            <Header />
             <Switch>
                 <Route path={'/'} exact component={HomePage} />
                 <Route path={'/shop'} exact component={ShopPagesComponent} />
